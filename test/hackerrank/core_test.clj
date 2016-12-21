173635b91ef1b51fed4fd5df20a38147e13b1dba
tkocmathla <matt.t.grimm@gmail.com>
2016-12-21 14:44:12 +0000

    Add tests for euler009

--- test/hackerrank/core_test.clj
+++ test/hackerrank/core_test.clj
@@ -1,3 +1,14 @@
 (ns hackerrank.core-test
-  (:require [clojure.test :refer :all]
-            [hackerrank.core :refer :all]))
+  (:require [clojure.string :as s]
+            [clojure.test :refer :all]
+            [hackerrank.core]))
+
+(deftest test-euler009 
+  (is (= "-1" (-> "1\n4"
+                  (with-in-str (hackerrank.euler009/solve))
+                  with-out-str
+                  s/trim)))
+  (is (= "60" (-> "1\n12"
+                  (with-in-str (hackerrank.euler009/solve))
+                  with-out-str
+                  s/trim))))
