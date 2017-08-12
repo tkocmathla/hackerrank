f0c3268719c6b5623251b1e6229443a5f3eeb727
tkocmathla <matt.t.grimm@gmail.com>
2017-08-12 07:37:31 -0600

    Delete old files

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject hackerrank "0.1.0-SNAPSHOT"
-  :description "HackerRank solutions"
-  :url "https://github.com/tkocmathla/hackerrank"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]]
-  :main ^:skip-aot hackerrank.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
--- test/hackerrank/core_test.clj
+++
@@ -1,1 +0,0 @@
-(ns hackerrank.core-test)
